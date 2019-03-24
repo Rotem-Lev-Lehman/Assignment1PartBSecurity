f0b5a6a2577cbe3c499ff35207c04df6d34e9eed
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2019-03-24 20:09:16 +0200

    finished RowsShifter

---
+++ src/RowsShifter.java
@@ -0,0 +1,43 @@
+public class RowsShifter {
+    public static byte[][] shiftRows(byte[][] arr){
+        byte[][] ans=new byte[arr.length][arr[0].length];
+        for(int rowNum=0;rowNum<ans.length;rowNum++){
+            for (int colNum=0;colNum<ans[0].length;colNum++){
+                int takeFrom=colNum+rowNum;
+                if(takeFrom>=ans[0].length)
+                    takeFrom=takeFrom-ans[0].length;
+                ans[rowNum][colNum]=arr[rowNum][takeFrom];
+            }
+        }
+        return ans;
+    }
+    public static void test(){
+        byte[][] test=new byte[4][4];
+        int counter=0;
+        System.out.println("Before shift:");
+        System.out.println("----------");
+        for(int rowNum=0;rowNum<test.length;rowNum++){
+            String print="";
+            for (int colNum=0;colNum<test[0].length;colNum++){
+                test[rowNum][colNum]=(byte)counter;
+                counter++;
+                print+=test[rowNum][colNum]+"-";
+            }
+            System.out.println(print.substring(0,print.length()-1));
+        }
+        System.out.println("----------");
+        System.out.println("After shift:");
+        System.out.println("----------");
+        test=shiftRows(test);
+        for(int rowNum=0;rowNum<test.length;rowNum++){
+            String print="";
+            for (int colNum=0;colNum<test[0].length;colNum++){
+                print+=test[rowNum][colNum]+"-";
+            }
+            System.out.println(print.substring(0,print.length()-1));
+        }
+        System.out.println("----------");
+    }
+}
+
+
