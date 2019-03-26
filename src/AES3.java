03be949ada95d0360f2ac2cfae66a2857c02c53c
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2019-03-26 14:48:53 +0200

    fixed AES3

--- src/AES3.java
+++ src/AES3.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
+
 public class AES3 implements IDecrypter, IEncrypter{
-    private AES1 aes1;
+    private ArrayList<AES1> aes;
 
     public AES3(Key k1, Key k2, Key k3) {
-        this.aes1 = new AES1(k1,k2,k3);
+        this.aes=new ArrayList<>();
+        aes.add(new AES1(k1));
+        aes.add(new AES1(k2));
+        aes.add(new AES1(k3));
     }
 
     @Override
     public PlainText Decrypt(CipherText cypherText) {
         CipherText curr = cypherText;
         for(int i = 0; i < 3; i++)
-            curr = new CipherText(aes1.Decrypt(curr));
+            curr = new CipherText(aes.get(aes.size()-i).Decrypt(curr));
 
         return new PlainText(curr);
     }
@@ -18,7 +23,7 @@ public class AES3 implements IDecrypter, IEncrypter{
     public CipherText Encrypt(PlainText message) {
         PlainText curr = message;
         for(int i = 0; i < 3; i++)
-            curr = new PlainText(aes1.Encrypt(curr));
+            curr = new PlainText(aes.get(i).Encrypt(curr));
 
         return new CipherText(curr);
     }
