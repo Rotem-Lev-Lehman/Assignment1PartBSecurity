b7d8a16e529b37c82e754c4d4106d49a0065dd31
Rotem-Lev-Lehman <rotem12430@gmail.com>
2019-03-26 15:26:18 +0200

    write done

--- src/BlocksFileWriter.java
+++ src/BlocksFileWriter.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class BlocksFileWriter extends MyFileWriter {
@@ -7,6 +10,11 @@ public class BlocksFileWriter extends MyFileWriter {
 
     @Override
     public void writeToFile(ArrayList<Block128> blocks) {
-
+        byte[] bytes = Block128.toByteArray(blocks);
+        try {
+            Files.write(Paths.get(path), bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
